ed908f6e011d1907c2149bbc1949e9137376d7ff
KHJ <herojun96@hanmail.net>
2023-02-23 16:29:06 +0900

    master 1

---
+++ mainCalc.py
@@ -0,0 +1,15 @@
+from add import add_func
+from sub import sub_func
+##함수
+
+
+##전역
+num1, num2, res = 100, 200, 0
+
+
+##메인
+res = add_func(num1,num2)
+print(res)
+
+res = sub_func(num1,num2)
+print(res)
